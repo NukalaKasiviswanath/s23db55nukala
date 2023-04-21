41029ceb3d221dc3117cfdfbd72ccb075286498c
NukalaKasiviswanath <123149800+NukalaKasiviswanath@users.noreply.github.com>
2023-04-21 01:06:36 -0500

    files

--- app.js
+++ app.js
@@ -3,7 +3,26 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+  Account.findOne({ username: username }, function (err, user) {
+  if (err) { return done(err); }
+  if (!user) {
+  return done(null, false, { message: 'Incorrect username.' });
+  }
+  if (!user.validPassword(password)) {
+  return done(null, false, { message: 'Incorrect password.' });
+  }
+  return done(null, user);
+  });
+  }));
+var Account =require('./models/account');
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
+  
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var electronicgadgetsRouter = require('./routes/electronicgadgets');
@@ -11,8 +30,8 @@ var boardRouter = require('./routes/board');
 var selectorRouter = require('./routes/selector');
 var resourceRouter = require('./routes/resource');
 
-var electronicgadgets = require("./models/electronicgadgets");
-
+var electronicgadgetsRouter = require("./models/electronicgadgets");
+var electronicgadgetsRouter = require("./controllers/electronicgadgets");
 var app = express();
 
 // view engine setup
@@ -23,6 +42,13 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+  }));
+  app.use(passport.initialize());
+  app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
